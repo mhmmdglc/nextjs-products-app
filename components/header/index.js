78318c553c989cd3375dc98f64f73321b99a476c
Muhammed Gulcu <muhammed.gulcu@gmail.com>
2022-03-18 07:34:34 +0300

    added search button

---
+++ components/header/index.js
@@ -0,0 +1,12 @@
+import style from "./style.module.scss"
+
+
+const Footer = () => {
+  return (
+      <p className={style.text}>
+          © Copyright 2022 Muhammed Gulcu. All rights reserved.
+      </p>
+  )
+}
+
+export default Footer
